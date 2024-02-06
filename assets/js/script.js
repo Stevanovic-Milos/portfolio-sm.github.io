fc9c680f670ccba99f2e7f6a77af76c1818050a1
Miloš Stevanović <129684157+Stevanovic-Milos@users.noreply.github.com>
2024-02-06 14:46:47 +0100

    some changest to arrange code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,26 @@
 'use strict';
 
-
-
-// element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-
-
-// sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
-
-
-// testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
-// modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
-// modal toggle function
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
-// add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
@@ -49,13 +36,9 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
-
-
-// custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
@@ -63,7 +46,6 @@ const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
 
-// add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
@@ -75,7 +57,6 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
-// filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
@@ -94,7 +75,6 @@ const filterFunc = function (selectedValue) {
 
 }
 
-// add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -113,14 +93,12 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
-  // contact form variables
+
   const form = document.querySelector("[data-form]");
   const formInputs = document.querySelectorAll("[data-form-input]");
   const formBtn = document.querySelector("[data-form-btn]");
 
-  // Function to check form validity and enable/disable submit button
   function checkFormValidity() {
     let isValid = true;
     formInputs.forEach(function(input) {
@@ -136,26 +114,21 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-  // Add event listeners to form input fields
   formInputs.forEach(function(input) {
     input.addEventListener("input", checkFormValidity);
   });
 
-  // Add event listener to form submit button
   formBtn.addEventListener("click", function() {
-    // Check form validity before submitting
+
     if (form.checkValidity()) {
       form.submit();
     }
   });
 });
 
-
-// page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
@@ -196,9 +169,9 @@ function animateText(index) {
       setTimeout(() => {
         lineElement.style.opacity = "1";
         animateText(index + 1);
-      }, 50); // Delay before revealing the next line
+      }, 50); 
     }
-  }, 20); // Speed of typing animation
+  }, 20); 
 }
 
 animateText(0);
