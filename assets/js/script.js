3944a6fbd4fb4dbe7cabb0c215aefa1dfdc1b27a
Stevanovic-Milos <stevanovicmilos0003@gmail.com>
2025-01-05 23:45:30 +0100

    changed rotation

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ avatar.addEventListener('click', () => {
     // Cycle through the images array
     currentIndex = (currentIndex + 1) % images.length; // This ensures it loops back to the first image
     avatar.src = images[currentIndex];
-  }, 300); // Wait until halfway through the 0.8s animation
+  }, 10); // Wait until halfway through the 0.8s animation
 
   // Remove the animation class after the animation ends
   avatar.addEventListener(
