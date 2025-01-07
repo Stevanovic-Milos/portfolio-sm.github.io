30a129660e7b4bfba9974663414d2beda9556fc0
Stevanovic-Milos <stevanovicmilos0003@gmail.com>
2025-01-07 07:51:48 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,178 +1,341 @@
-const elementToggleFunc = (elem) => elem.classList.toggle("active");
+'use strict';
 
-// Sidebar toggle
+// Ensure that the function is declared only once
+const elementToggleFunc = function(elem) {
+  elem.classList.toggle("active");
+};
+
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
-sidebarBtn?.addEventListener("click", () => elementToggleFunc(sidebar));
 
-// Testimonials modal
+sidebarBtn.addEventListener("click", function() {
+  elementToggleFunc(sidebar);
+});
+
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
+
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
-const toggleModal = () => {
+const testimonialsModalFunc = function() {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 };
 
-testimonialsItem.forEach((item) =>
-  item.addEventListener("click", () => {
-    modalImg.src = item.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = item.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = item.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = item.querySelector("[data-testimonials-text]").innerHTML;
-    toggleModal();
-  })
-);
+// Loop to handle testimonial item clicks
+for (let i = 0; i < testimonialsItem.length; i++) {
+  testimonialsItem[i].addEventListener("click", function() {
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    
+    testimonialsModalFunc();
+  });
+}
 
-modalCloseBtn?.addEventListener("click", toggleModal);
-overlay?.addEventListener("click", toggleModal);
+// Event listener to close modal
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
 
-// Dropdown filter
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
-const filterItems = document.querySelectorAll("[data-filter-item]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-const filterFunc = (selectedValue) => {
-  filterItems.forEach((item) => {
-    const matches = selectedValue === "all" || selectedValue === item.dataset.category;
-    item.classList.toggle("active", matches);
-  });
-};
+// Toggle the dropdown
+select.addEventListener("click", function() {
+  elementToggleFunc(this);
+});
 
-select?.addEventListener("click", () => elementToggleFunc(select));
-
-selectItems.forEach((item) =>
-  item.addEventListener("click", () => {
-    const value = item.innerText.toLowerCase();
-    selectValue.innerText = item.innerText;
+// Loop through select items to change the selected value
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function() {
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
     elementToggleFunc(select);
-    filterFunc(value);
-  })
-);
+    filterFunc(selectedValue);
+  });
+}
+
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function(selectedValue) {
+
+  for (let i = 0; i < filterItems.length; i++) {
+
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+
+  }
+
+}
 
 let lastClickedBtn = filterBtn[0];
-filterBtn.forEach((btn) =>
-  btn.addEventListener("click", () => {
-    const value = btn.innerText.toLowerCase();
-    selectValue.innerText = btn.innerText;
-    filterFunc(value);
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function() {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
 
     lastClickedBtn.classList.remove("active");
-    btn.classList.add("active");
-    lastClickedBtn = btn;
-  })
-);
+    this.classList.add("active");
+    lastClickedBtn = this;
 
-// Form validation
-document.addEventListener("DOMContentLoaded", () => {
+  });
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
   const form = document.querySelector("[data-form]");
   const formInputs = document.querySelectorAll("[data-form-input]");
   const formBtn = document.querySelector("[data-form-btn]");
 
-  if (form) {
-    const checkFormValidity = () => {
-      const isValid = [...formInputs].every((input) => input.validity.valid);
-      formBtn.disabled = !isValid;
-    };
+  function checkFormValidity() {
+    let isValid = true;
+    formInputs.forEach(function(input) {
+      if (!input.validity.valid) {
+        isValid = false;
+      }
+    });
 
-    formInputs.forEach((input) => input.addEventListener("input", checkFormValidity));
-
-    formBtn.addEventListener("click", () => form.checkValidity() && form.submit());
+    if (isValid) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
   }
+
+  formInputs.forEach(function(input) {
+    input.addEventListener("input", checkFormValidity);
+  });
+
+  formBtn.addEventListener("click", function() {
+
+    if (form.checkValidity()) {
+      form.submit();
+    }
+  });
 });
 
-// Navigation links
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-navigationLinks.forEach((link) =>
-  link.addEventListener("click", () => {
-    pages.forEach((page, index) => {
-      const isActive = link.innerHTML.toLowerCase() === page.dataset.page;
-      page.classList.toggle("active", isActive);
-      navigationLinks[index].classList.toggle("active", isActive);
-    });
-    window.scrollTo(0, 0);
-  })
-);
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function() {
 
-// Text animation
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
+  });
+}
+
 const textLines = [
   "Success is not just about doing things; it's about doing them right.",
   " Hi there! I'm Milos, a diligent student with a passion for learning and a commitment to excellence. Welcome to my portfolio, where you'll find a showcase of my journey and capabilities. Let's connect and explore how I can contribute to your team's success."
 ];
+
 const lineElements = document.querySelectorAll(".about-text p");
 
-if (window.innerWidth > 768) {
-  const animateText = (index) => {
-    if (index >= textLines.length) return;
+function animateText(index) {
+  if (index >= textLines.length) return;
 
-    let charIndex = 0;
-    const lineElement = lineElements[index];
-    const intervalId = setInterval(() => {
-      lineElement.textContent = textLines[index].slice(0, charIndex++);
-      if (charIndex > textLines[index].length) {
-        clearInterval(intervalId);
-        setTimeout(() => {
-          lineElement.style.opacity = "1";
-          animateText(index + 1);
-        }, 50);
-      }
-    }, 20);
-  };
+  const currentLine = textLines[index];
+  const lineElement = lineElements[index];
+
+  let charIndex = 0;
+  const intervalId = setInterval(() => {
+    if (charIndex <= currentLine.length) {
+      lineElement.textContent = currentLine.slice(0, charIndex);
+      charIndex++;
+    } else {
+      clearInterval(intervalId);
+      setTimeout(() => {
+        lineElement.style.opacity = "1";
+        animateText(index + 1);
+      }, 50);
+    }
+  }, 20);
+}
+
+// Check if the screen size is smaller than 768px (adjust as needed)
+if (window.innerWidth > 768) {
+  // Only run typing animation for larger screens
   animateText(0);
 } else {
+  // For small screens, immediately display the full text without animation
   lineElements.forEach((lineElement, index) => {
     lineElement.textContent = textLines[index];
-    lineElement.style.opacity = "1";
+    lineElement.style.opacity = "1"; // Ensure text is visible
   });
 }
 
-// Download portfolio
-const downloadPortfolio = () => {
-  const link = document.createElement("a");
-  link.href = "portfolioMS.pdf";
-  link.download = "portfolioMS.pdf";
-  link.click();
-};
 
-// Intersection observer for animations
-const observeElements = (selector, threshold = 0.5, stagger = 0) => {
-  const elements = document.querySelectorAll(selector);
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry, index) => {
-      if (entry.isIntersecting) {
-        setTimeout(() => entry.target.classList.add("visible"), index * stagger);
+function downloadPortfolio() {
+  const link = document.createElement('a');
+  link.href = 'portfolioMS.pdf'; // Path to the portfolio file
+  link.download = 'portfolioMS.pdf'; // The name the file will have when downloaded
+  link.click(); // Simulates a click to trigger the download
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const serviceItems = document.querySelectorAll(".service-item");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target); // Stop observing once it's visible
+        }
+      });
+    }, {
+      threshold: 0., // Trigger when 20% of the element is visible
+    }
+  );
+
+  serviceItems.forEach((item) => {
+    observer.observe(item);
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const testimonials = document.querySelectorAll(".testimonials-item");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, {
+      threshold: 0.5, // Adjust threshold as needed
+    }
+  );
+
+  testimonials.forEach((testimonial) => {
+    observer.observe(testimonial);
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const timelineItems = document.querySelectorAll(".timeline-item");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, {
+      threshold: 0.5, // Adjust to control when the animation starts (50% visibility)
+    }
+  );
+
+  timelineItems.forEach((item) => {
+    observer.observe(item);
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const serviceItems = document.querySelectorAll(".service-item-cer");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+          // Add `visible` class with delay for each row
+          setTimeout(() => {
+            entry.target.classList.add("visible");
+          }, index * 150); // Stagger delay between items
+        }
+      });
+    }, {
+      threshold: 0.1, // Adjust to control when the animation starts
+    }
+  );
+
+  serviceItems.forEach((item) => {
+    observer.observe(item);
+  });
+});
+window.addEventListener('load', function() {
+  particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 100, // Reduce particle count for mobile
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      size: {
+        value: 3, // Adjust particle size
+        random: true
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        out_mode: "out"
       }
-    });
-  }, { threshold });
+    },
 
-  elements.forEach((el) => observer.observe(el));
-};
+    retina_detect: true
+  });
+  
+});
+const button = document.querySelector('.info_more-btn');
+button.addEventListener('click', () => {
+  button.classList.toggle('opened');
+});
 
-observeElements(".service-item", 0.1);
-observeElements(".testimonials-item", 0.5);
-observeElements(".timeline-item", 0.5);
-observeElements(".service-item-cer", 0.1, 150);
+/*image rotation*/
+// Ensure 'avatar' is only declared once
+const avatar = document.getElementById('avatar');
+let currentIndex = 0; // Track the current image index
 
-// Image rotation
-const avatar = document.getElementById("avatar");
-const images = ["./assets/images/ME2.webp", "./assets/images/me.jpg"];
-let currentIndex = 0;
+// Define the image paths
+const images = ['./assets/images/ME2.webp','./assets/images/me.jpg'];
 
-avatar?.addEventListener("click", () => {
-  avatar.classList.add("animate");
-  currentIndex = (currentIndex + 1) % images.length;
+avatar.addEventListener('click', () => {
+  // Add animation class to trigger the spin
+  avatar.classList.add('animate');
+
+  // Wait for the animation duration to swap the image
   setTimeout(() => {
+    // Cycle through the images array
+    currentIndex = (currentIndex + 1) % images.length; // This ensures it loops back to the first image
     avatar.src = images[currentIndex];
-  }, 100);
+  }, 10); // Wait until halfway through the 0.8s animation
 
-  avatar.addEventListener("animationend", () => avatar.classList.remove("animate"), { once: true });
+  // Remove the animation class after the animation ends
+  avatar.addEventListener(
+    'animationend',
+    () => {
+      avatar.classList.remove('animate');
+    },
+    { once: true } // Ensures the event listener is removed after firing
+  );
 });
