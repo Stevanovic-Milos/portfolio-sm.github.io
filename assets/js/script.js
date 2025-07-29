0c03454a8deec2f987e0caa67d803db471936f2a
Milos Stevanovic <stevanovicmilos0003@gmail.com>
2025-07-29 23:00:51 +0200

    izmenjeni stilovi i izgled projekata

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,20 +63,15 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function(selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+    const category = filterItems[i].dataset.category.toLowerCase();
+    if (selectedValue === "all" || selectedValue.toLowerCase() === category) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
-
-}
+};
 
 let lastClickedBtn = filterBtn[0];
 
